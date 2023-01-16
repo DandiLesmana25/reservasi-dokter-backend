99af862e5c2edf98b831fbec1af1f480d59c9229
Dandi Lesmana <dandilesmana.if21@gmail.com>
2023-01-16 06:08:45 -0800

    add migration table

---
+++ database/migrations/2023_01_15_135200_create_detail_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_users', function (Blueprint $table) {
+            $table->id();
+            $table->integer('users_id');
+            $table->integer('type_users_id')->nullable();
+            $table->string('contact')->nullable();
+            $table->longText('address')->nullable();
+            $table->longText('photo')->nullable();
+            $table->enum('gender', [1,2])->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_users');
+    }
+}
